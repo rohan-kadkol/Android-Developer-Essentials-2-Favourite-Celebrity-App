428887c25fbdf7e895047f68d8cf9ae6197be302
Rohan Kadkol <rohankd100@gmail.com>
2020-06-24 17:02:58 -0500

    Solution Code

--- app/src/main/java/com/rohankadkol/favouritecelebrityapp2/MainActivity.java
+++ app/src/main/java/com/rohankadkol/favouritecelebrityapp2/MainActivity.java
@@ -3,12 +3,20 @@ package com.rohankadkol.favouritecelebrityapp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View view) {
+        Toast.makeText(this, "Hello Rohan!", Toast.LENGTH_SHORT).show();
     }
 }
